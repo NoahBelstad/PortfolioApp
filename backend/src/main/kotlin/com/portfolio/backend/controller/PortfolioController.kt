67638dc8996183c51d1ee918de8e8a913c88e948
Noah <noahbelstad@gmail.com>
2026-03-19 17:58:58 +0100

    Added fetching data from Github and fixed gitignore

---
+++ backend/src/main/kotlin/com/portfolio/backend/controller/PortfolioController.kt
@@ -0,0 +1,19 @@
+package com.portfolio.backend.controller
+
+import com.portfolio.backend.models.Projetc
+import com.portfolio.backend.service.PortfolioService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("/api/v1/github")
+class PortfolioController(private val service: PortfolioService) {
+
+    @GetMapping("/{username}")
+    fun getProjetcs(@PathVariable username: String): List<Projetc > {
+        return service.getGithubRepos(username)
+    }
+}
